7199f595d23ccacbe073e4635aaeaf2c2419d9fc
Obrienmaina-Mosbach <191464653+Obrienmaina-Mosbach@users.noreply.github.com>
2024-12-14 08:13:50 +0100

    Added Second Name

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 print('Hello World')
 
 firstName = input('What is your first name?')
-print('Your first name is ' + firstName + '.' )
+secondName = input('What is your second name?')
+print('Your name is ' + firstName + ' ' + secondName + '.' )
