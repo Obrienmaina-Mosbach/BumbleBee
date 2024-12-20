4ed086b87d95e544cc0148dc203c3e47ced925eb
Obrienmaina-Mosbach <191464653+Obrienmaina-Mosbach@users.noreply.github.com>
2024-12-20 11:25:49 +0100

    Folder organization stuff

--- for Loops 20 December 2024/DotProduct.java
+++ for Loops 20 December 2024/DotProduct.java
@@ -3,11 +3,11 @@ public class DotProduct {
         int[] A = {1, 2, 3};
         int[] B = {4, 5, 6};
 
-        int result = 0;
+        int DotProduct = 0;
         for (int i = 0; i < A.length; i++) {
-            result += A[i] * B[i];
+            DotProduct += A[i] * B[i];
         }
 
-        System.out.println("Dot product: " + result);
+        System.out.println("Dot product: " + DotProduct);
     }
 }
