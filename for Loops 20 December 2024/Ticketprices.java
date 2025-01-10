22d05fc047865b299843c467fb5304b81babc9a5
Obrienmaina-Mosbach <191464653+Obrienmaina-Mosbach@users.noreply.github.com>
2025-01-10 10:06:01 +0100

    varying ages

--- for Loops 20 December 2024/Ticketprices.java
+++ for Loops 20 December 2024/Ticketprices.java
@@ -3,15 +3,25 @@ import java.util.Scanner;
 
 public class Ticketprices {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("What is your age? ");
-       int age = Integer.parseInt(scanner.nextLine());
+        int Price = 100;
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("What is your age? ");
+        int age = Integer.parseInt(scnr.nextLine());
     
-        if (age <= 30){
-            System.out.println("You get a discount");
+        if (age == 0){
+            System.out.println("Enter a valid age");
+        }
+        else if(age <= 10){
+            System.out.println("You get a 30% discount: " + "The new product price is Euro " + Price * 0.7);
+        }
+        else if(age <= 20){
+            System.out.println("You get a 20% discount: " + "The new product price is Euro " + Price * 0.8);
+        }
+        else if(age <= 30){
+            System.out.println("You get a 10% discount: " + "The new product price is Euro " + Price * 0.9);
         }
         else{
-            System.out.println("You dont qualify for a discount");
+            System.out.println("You don't qualify for a discount");
         }
     }
 }
